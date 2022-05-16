d57da2710c5aeebbefdda186df431a24ffea85ed
Nithin K Joy <nithinjoy121212@gmail.com>
2022-05-16 18:49:44 +0530

    removed .env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 require("./startup/db")();
 
-require("dotenv").config();
+// require("dotenv").config();
 require("./startup/cors")(app);
 require("./startup/routes")(app);
 
