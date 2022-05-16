64e3a29f27c64a32f8abdd7bb6185276d4f950d7
Nithin K Joy <nithinjoy121212@gmail.com>
2022-05-16 18:58:16 +0530

    added mongoose url

--- startup/db.js
+++ startup/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 module.exports = () => {
   mongoose
-    .connect("mongodb://127.0.0.1:27017/easypg",{
+    .connect(process.env.mongoDBKey,{
         useUnifiedTopology: true,
         useNewUrlParser: true,
     })
